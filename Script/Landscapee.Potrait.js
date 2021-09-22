82331184371d782eaf2281ea8f7adfd2d80abc6c
NurAlskorry <Skorrycloud@gmail.com>
2021-09-22 09:07:13 -0600

    Update Landscapee.Potrait.js

--- Script/Landscapee.Potrait.js
+++ Script/Landscapee.Potrait.js
@@ -1,1 +1,25 @@
-console.log("you made ")
+console.log("You made it to the LandScapePortrait.js!");
+
+let stop = false; //Boolean for testNaN() validation function, see end of file
+
+function javascript () { //Where colsole.log tested file goes
+
+}
+//Main Function, necessary lines of code
+function main() {
+  let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
+  //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
+  firstNumber = document.getElementById("textField1").value;
+  //Validate by alert or HTML Text in p-tag
+  document.getElementById("validityTest1").innerHTML = alert (testNAN (firstNumber) );
+}
+//
+function testNaN (number) {
+  if ( isNaN (number) ) { //NaN are not values o REAL Number System
+    stop = true;
+    return "Type a Real Number"
+  } else {
+    return "Input Validated"
+  }
+
+}
